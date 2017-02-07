277992b54d4e5e35f8bb73e141cb6a8e89424a7c
Mansur Marvanov <nanorobocop@gmail.com>
2017-02-07 07:28:19 +0300

    added exception handling, stat output and restart from last to prepare_db

--- prepare_db.py
+++ prepare_db.py
@@ -16,17 +16,31 @@
 checking_params = { "ping": 1, "port25": 1, "port80": 1 }
 db = '/mnt/storage/share/projects/ips_db/data/ips_integer.db'
 
-def ips_generator():
-    for ip in range(255*255*255*255):
+def ips_generator(cmax):
+    for ip in range(cmax, 255*255*255*255):
+    #for ip in range(255*255):
         yield (ip,)
+
+def num2ip(ip_num):
+    oct3 = ip_num // ( 256 * 256 * 256 ) % 256
+    oct2 = ip_num // ( 256 * 256 ) % 256
+    oct1 = ip_num // ( 256 ) % 256
+    oct0 = ip_num % 256
+    return("{0}.{1}.{2}.{3}".format(oct3, oct2, oct1, oct0))
 
 def prepare_db():
     import sqlite3
     conn = sqlite3.connect(db)
     c = conn.cursor()
+    cmax = c.execute('''select ip from ips_db order by ip desc limit 1''').fetchone()[0]
+    print("Current maximum IP num: {0}".format(cmax))
+    print("Current maximum IP: {0}".format(num2ip(cmax)))
     c.execute('''CREATE TABLE IF NOT EXISTS ips_db (ip integer primary key, ping integer, port25 integer, port80 integer)''')
     conn.commit()
-    c.executemany("""insert into ips_db ('ip') values (?)""", ips_generator())
+    try:
+        c.executemany("""insert or ignore into ips_db ('ip') values (?)""", ips_generator(cmax))
+    except:
+        conn.commit()
     conn.commit()
 
 prepare_db()
